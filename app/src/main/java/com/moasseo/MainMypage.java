886cb0a4cb0af6c8c04c114d47e9e0c91dd7b593
audtn <audtncks77@naver.com>
2022-06-09 14:11:07 +0900

    내정보 >시장 별 포인트 화면

--- app/src/main/java/com/moasseo/MainMypage.java
+++ app/src/main/java/com/moasseo/MainMypage.java
@@ -1,5 +1,6 @@
 package com.moasseo;
 
+import android.content.Intent;
 import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +12,7 @@ public class MainMypage extends MainActivity{
 
     TextView grade_info;  //등급안내 Text
     ConstraintLayout grade_pop;  //팝업 레이아웃
+    ConstraintLayout mypage_point;  //시장 별 포인트
     View close;  //하단 닫기 뷰
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +21,7 @@ public class MainMypage extends MainActivity{
 
         grade_info = (TextView) findViewById(R.id.textView36);  //등급안내 Text
         grade_pop = (ConstraintLayout) findViewById(R.id.constraintlayout10);  //팝업 레이아웃
+        mypage_point = (ConstraintLayout) findViewById(R.id.constraintLayout5);  //내 정보 - 시장 별 포인트
         close = (View) findViewById(R.id.view3);  //하단 닫기 뷰
 
         grade_info.setPaintFlags(grade_info.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);  //등급안내 Text 밑줄
@@ -34,6 +37,15 @@ public class MainMypage extends MainActivity{
             @Override
             public void onClick(View v) {
                 grade_pop.setVisibility(View.GONE);  //팝업 레이아웃(모아써 등급 표시) 꺼짐
+            }
+        });
+
+        mypage_point.setOnClickListener(new View.OnClickListener() {  //시장 별 포인트 레이아웃을 클릭하면
+            @Override
+            public void onClick(View v) {
+                //시장별 포인트로 넘어가기
+                Intent intent = new Intent(MainMypage.this, MainMypagePoint.class);  //시장 별 포인트 화면으로 이동
+                startActivity(intent);
             }
         });
     }
