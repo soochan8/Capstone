e6fda0ccadd7d13094f655193f2df67bd5719efe
audtn <audtncks77@naver.com>
2022-06-09 12:52:31 +0900

    내 정보_모아써등급 팝업

--- app/src/main/java/com/moasseo/MainMypage.java
+++ app/src/main/java/com/moasseo/MainMypage.java
@@ -1,10 +1,40 @@
 package com.moasseo;
 
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class MainMypage extends MainActivity{
+
+    TextView grade_info;  //등급안내 Text
+    ConstraintLayout grade_pop;  //팝업 레이아웃
+    View close;  //하단 닫기 뷰
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mypage);
+
+        grade_info = (TextView) findViewById(R.id.textView36);  //등급안내 Text
+        grade_pop = (ConstraintLayout) findViewById(R.id.constraintlayout10);  //팝업 레이아웃
+        close = (View) findViewById(R.id.view3);  //하단 닫기 뷰
+
+        grade_info.setPaintFlags(grade_info.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);  //등급안내 Text 밑줄
+
+        grade_info.setOnClickListener(new View.OnClickListener() {  //등급안내 Text 클릭 시
+            @Override
+            public void onClick(View v) {
+                grade_pop.setVisibility(View.VISIBLE);  //팝업 레이아웃(모아써 등급 표시)
+            }
+        });
+
+        close.setOnClickListener(new View.OnClickListener() {  //닫기 뷰 클릭 시
+            @Override
+            public void onClick(View v) {
+                grade_pop.setVisibility(View.GONE);  //팝업 레이아웃(모아써 등급 표시) 꺼짐
+            }
+        });
     }
 }
